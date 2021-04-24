f7328dafd45880a35bc9e427dfef4699c95980dd
danieldvoranov <danidvoryanov@gmail.com>
2021-04-24 14:39:52 +0300

    изменение12

---
+++ src/test/java/lessons/lesson1/exampl1/Radius.java
@@ -0,0 +1,15 @@
+package lessons.lesson1.exampl1;
+
+public class Radius {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int  b= 1;
+
+
+        System.out.println("HelloDaniel");
+        System.out.println(a);
+
+    }
+}
